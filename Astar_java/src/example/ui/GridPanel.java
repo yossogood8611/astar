0715c7afd2d574a5c6ae4dd23dacc97ac4f710f3
이유진 <rooproop1111@naver.com>
2023-05-16 20:57:00 +0900

    add player moving
    - keyPressed

--- Astar_java/src/example/ui/GridPanel.java
+++ Astar_java/src/example/ui/GridPanel.java
@@ -3,6 +3,8 @@ package example.ui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
@@ -41,6 +43,45 @@ public class GridPanel extends JPanel implements Observer {
         this.widerStroke = new BasicStroke(2);
 
         setBorder(new LineBorder(Color.gray));
+
+
+        addKeyListener(new KeyListener() { // 키보드 이벤트 리스너 추가
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+                if (keyCode == KeyEvent.VK_UP) { // 위쪽 화살표 키
+                    if (end != null) {
+                        end = new Tile(end.getX(), end.getY()-1);
+                        repaint();
+                    }
+                } else if (keyCode == KeyEvent.VK_DOWN) { // 아래쪽 화살표 키
+                    if (end != null) {
+                        end = new Tile(end.getX(), end.getY()+1);
+                        repaint();
+                    }
+                } else if (keyCode == KeyEvent.VK_LEFT) { // 왼쪽 화살표 키
+                    if (end != null) {
+                        end = new Tile(end.getX()-1, end.getY());
+                        repaint();
+                    }
+                } else if (keyCode == KeyEvent.VK_RIGHT) { // 오른쪽 화살표 키
+                    if (end != null) {
+                        end = new Tile(end.getX()+1, end.getY());
+                        repaint();
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
+        setFocusable(true); // 키보드 입력을 받기 위해 포커스 설정
 
         addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
